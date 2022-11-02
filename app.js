86cf51bb4a6a6468985a5d2257abee45a6545a62
Syahirjalel <115330123+Syahirjalel@users.noreply.github.com>
2022-11-02 21:01:50 +0800

    faiz karipap
    karipap

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get("/", function(req,res) {
-    res.send("Faizzzz");
+    res.send("faiz cipap");
 })
 
 app.listen(3000,function() {
